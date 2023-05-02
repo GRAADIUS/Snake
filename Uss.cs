3278edb4386a1ebe99143ffea22bbcac53a796b6
Egor Fedorenko <egorfedorenko29@gmail.com>
2023-05-02 11:17:01 +0300

    Update class "point": add full automatic

--- Uss.cs
+++ Uss.cs
@@ -11,16 +11,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            point p1 = new point();
-            p1.x= 1;
-            p1.y= 2;
-            p1.sym = '=';
+            point p1 = new point(1, 3, '=');
             p1.Draw();
 
-            point p2 = new point();
-            p2.x = 2;
-            p2.y = 2;
-            p2.sym = ';';
+            point p2 = new point(1, 4, '=');
             p2.Draw();
         }
     }
