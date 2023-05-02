6c936788dd22d42b122a443d0c04b244124669ff
Egor Fedorenko <egorfedorenko29@gmail.com>
2023-05-02 10:59:23 +0300

    Class "Point"; "Draw" in Class

--- Uss.cs
+++ Uss.cs
@@ -11,22 +11,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
+            point p1 = new point();
+            p1.x= 1;
+            p1.y= 2;
+            p1.sym = '=';
+            p1.Draw();
 
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            point p2 = new point();
+            p2.x = 2;
+            p2.y = 2;
+            p2.sym = ';';
+            p2.Draw();
         }
     }
 }
