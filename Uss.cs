16d8d84c639cbd1aaeaa373641a4a246a2d05436
opilane <opilane@HP-CZC2349HTG>
2023-05-15 14:56:11 +0300

    Loeng12_C#(eda)

--- Uss.cs
+++ Uss.cs
@@ -11,31 +11,45 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(80, 25);
-            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(50, 25);
+            Console.SetBufferSize(50, 25);
 
-            HorisontalLine upLine = new HorisontalLine(0, 78, 0, '+');
+            HorisontalLine upLine = new HorisontalLine(0, 48, 0, '¤');
             upLine.Drow();
-            HorisontalLine downLine = new HorisontalLine(0, 78, 24, '+');
+            HorisontalLine downLine = new HorisontalLine(0, 48, 24, '¤');
             downLine.Drow();
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '¤');
             leftLine.Drow();
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 48, '¤');
             rightLine.Drow();
             
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow(); //
 
+            FoodCreator foodCreator = new FoodCreator(20, 20, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
         }
     }
--- point.cs
+++ point.cs
@@ -46,6 +46,13 @@ namespace Snake
             }
         }
 
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
+
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
